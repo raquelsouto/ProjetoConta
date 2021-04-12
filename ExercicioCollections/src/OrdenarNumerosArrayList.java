4da453fdc34506ddb9085f6338e217ea1f282834
Raquel Souto <kelsouto@gmail.com>
2021-04-12 10:56:28 -0300

    Adicionar comentarios ao projeto

--- ExercicioCollections/src/OrdenarNumerosArrayList.java
+++ ExercicioCollections/src/OrdenarNumerosArrayList.java
@@ -12,13 +12,17 @@ public class OrdenarNumerosArrayList {
             listaNumeros.add(j);
         }
         
-        //Colocando em ordem Decrescente
+        System.out.println("EM ORDEM CRESCENTE");
+        Collections.sort(listaNumeros);
+        System.out.println(listaNumeros);
+        
+        //Modo1: Colocando em ordem Decrescente
         Collections.reverse(listaNumeros);
         
-        for (Integer integer : listaNumeros) {
-            System.out.println(integer);
+      //Modo2: Colocando em ordem Decrescente
+        Collections.sort(listaNumeros, Collections.reverseOrder());
+        System.out.println("EM ORDEM DECRESCENTE");
+        System.out.println(listaNumeros);
         }
         
-    }
-
 }
