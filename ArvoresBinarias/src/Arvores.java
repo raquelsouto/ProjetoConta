150b6dac544c848f6c07f18295737b102524772d
Raquel Souto <kelsouto@gmail.com>
2021-05-21 11:06:34 -0300

    Adicionar exercicio9

--- ArvoresBinarias/src/Arvores.java
+++ ArvoresBinarias/src/Arvores.java
@@ -3,16 +3,7 @@ public class Arvores {
 
     private int folha;
 
-    public void preOrdem(No no) {
-        if(no != null) {
-            System.out.println(no.getCodigo());
-            preOrdem(no.getEsquerda());
-            preOrdem(no.getDireita());
-        }
-    }
-
     public int quantidadeFolhas(No no) {
-        preOrdem(no);
         
         if(no.getDireita() == null && no.getDireita() == null) {
             folha++;
